6f835e7d35cb941246c0aa2692b89d69219a6445
4seokhyeon <tktjrgus@naver.com>
2024-01-08 18:48:22 +0900

    [binding 관련 커밋]현재 LoginResultActivity 의 클래스에서 binding이 잘못 선언이 되어있어서 수정을 했습니다. 그리고 커밋 메시지도 자세하게 좀 남겨주세요 뭘 했는지 한눈에 파악하기 힘들어요 그리고 기능이 같은 클래스들은 하나의 패키지안에 넣어주세요 ex) Login관련 (회원가입)등등 으로 나눌수 있겠습니다.

--- app/src/main/java/com/subway/railme/LoginResultActivity.java
+++ app/src/main/java/com/subway/railme/LoginResultActivity.java
@@ -10,23 +10,24 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 // 오류 수정 예정
-import com.subway.railme.databinding.ActivityLoginResultBinding;
+import com.subway.railme.databinding.ActivityJoinBinding;
+
 
 // 로그인에서 입력 받은 값 요청 받는 클래스
+// 바인딩을 쓰는데 왜 findviewbtid를 쓰는지 알려주실수있나요
 public class LoginResultActivity extends AppCompatActivity {
 
-    private ActivityLoginResultBinding binding;
+    private ActivityJoinBinding binding;//여기도 이상한 xml 바인딩 되어있길래 바꿨습니다. 확인해서 수정해주세요
     EditText EditText_get;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityLoginResultBinding.inflate(getLayoutInflater());
+        binding = ActivityJoinBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
-        EditText_get = findViewById(R.id.EditText_get);
+        binding.etJoinId.setText(ID + "|"); // 이런식으로 수정해주세요
 
         Intent intent = getIntent();
 
@@ -34,7 +35,7 @@ public class LoginResultActivity extends AppCompatActivity {
         String UseId = bundle.getString("ID");
         String password = bundle.getString("Password");
 
-        EditText_get.setText(ID + "|" + Password);
+        binding.etJoinPassword.setText(ID + "|");
 
     }
 }
