9b549a7eecd62d4b4b8b6fbbe4f9189dddf4bf61
4SeokHyeon <tktjrgus@naver.com>
2024-03-26 11:09:46 +0900

    binding 변수 자체는 뭘 쓰던 마음인데 언더바는 앞에 쓰세요_binding 이런 식으로

--- app/src/main/java/com/subway/railme/mypage/MpTimeActivity.java
+++ app/src/main/java/com/subway/railme/mypage/MpTimeActivity.java
@@ -7,25 +7,19 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.subway.railme.R;
-import com.subway.railme.databinding.ActivityLoginBinding;
-import com.subway.railme.databinding.ActivityMainBinding;
 import com.subway.railme.databinding.ActivityMpTimeBinding;
 import com.subway.railme.home.HomeFragment;
 
 public class MpTimeActivity extends AppCompatActivity {
 
-    private ActivityMpTimeBinding binding_t;
+    private ActivityMpTimeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding_t = ActivityMpTimeBinding.inflate(getLayoutInflater());
-        setContentView(binding_t.getRoot());
+        binding = ActivityMpTimeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        HomeFragment homeFragment = new HomeFragment();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.add(R.layout.frameLayout, fragment);
-        fragmentTransaction.commit();
     }
 
     //@Override
