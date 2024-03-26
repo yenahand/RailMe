464dc68902184985dce339ac5b11c2c5835b28ec
4SeokHyeon <tktjrgus@naver.com>
2024-03-26 11:10:08 +0900

    oncreate에 해쉬키를 뽑는 코드 추가함

--- app/src/main/java/com/subway/railme/MainActivity.java
+++ app/src/main/java/com/subway/railme/MainActivity.java
@@ -1,6 +1,11 @@
 package com.subway.railme;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +17,9 @@ import androidx.navigation.ui.NavigationUI;
 import com.subway.railme.databinding.ActivityMainBinding;
 import com.subway.railme.databinding.FragmentMyPageBinding;
 import com.subway.railme.mypage.MyPageFragment;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         _binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = _binding.getRoot();
         setContentView(view);
+        Log.d("GetKey",getKeyHash());
 
         //Component 네비게이션 바텀 하단바 적용
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.my_nav_host);
@@ -34,6 +43,24 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    private String getKeyHash() {  try{
+        PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        if(packageInfo == null) return null;
+        for(Signature signature: packageInfo.signatures){
+            try{
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                return android.util.Base64.encodeToString(md.digest(), Base64.NO_WRAP);
+            }catch (NoSuchAlgorithmException e){
+                Log.w("getKeyHash", "Unable to get MessageDigest. signature="+signature, e);
+            }
+        }
+    }catch(PackageManager.NameNotFoundException e){
+        Log.w("getPackageInfo", "Unable to getPackageInfo");
+    }
+        return null;
     }
 
     @Override
