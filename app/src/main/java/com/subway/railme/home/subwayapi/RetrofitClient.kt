062c406087a157c0919654bfcf8b3e4a4bfdbae2
4SeokHyeon <tktjrgus@naver.com>
2024-03-26 16:27:31 +0900

    레트로핏 빌더 작성

---
+++ app/src/main/java/com/subway/railme/home/subwayapi/RetrofitClient.kt
@@ -0,0 +1,31 @@
+package com.subway.railme.home.subwayapi
+
+import com.subway.railme.Unit.API
+import com.subway.railme.Unit.BASE_URL
+import com.tickaroo.tikxml.TikXml
+import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    val api: RetrofitInterface get() = instance.create(RetrofitInterface::class.java)
+
+    private val instance: Retrofit
+        get() {
+            val httpClient = OkHttpClient.Builder().addInterceptor { chain ->
+                val request: Request = chain.request()
+                    .newBuilder()
+                    .addHeader("Authorization", API)
+                    .build()
+                chain.proceed(request)
+            }.build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(TikXmlConverterFactory.create(TikXml.Builder().exceptionOnUnreadXml(false).build()))
+                .build()
+        }
+
+}
