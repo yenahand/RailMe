cd4c9a2bb44981dc107edbc60e1f8f88cbe92261
4SeokHyeon <tktjrgus@naver.com>
2024-03-26 11:11:49 +0900

    kakao SDK V2를 위한 인증키 초기화 클래스명 변경 검색할떄 V2 버전으로 업데이트한 블로그 보세여

---
+++ app/src/main/java/com/subway/railme/login/GlobalApplication.java
@@ -0,0 +1,30 @@
+package com.subway.railme.login;
+
+import android.app.Application;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import com.kakao.sdk.common.KakaoSdk;
+
+
+public class GlobalApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        /**
+         * 카카오 SDK V2
+         */
+        String kakaoAppkey = "8c38182120bfba3a8e9b9cc350ff986b";
+        try {
+            ApplicationInfo ai = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            if (ai.metaData != null) {
+                kakaoAppkey = ai.metaData.getString("com.kakao.sdk.AppKey");
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            // if we can't find it in the manifest, just return null
+        }
+        KakaoSdk.init(this, kakaoAppkey);
+    }
+
+
+}
