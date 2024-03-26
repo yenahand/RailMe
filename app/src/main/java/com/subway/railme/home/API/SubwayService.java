49b53181ec4314eb85559510a66395d5bd7a5da8
4SeokHyeon <tktjrgus@naver.com>
2024-03-26 16:26:14 +0900

    Api 인터페이스 기존 Path로 변경

--- app/src/main/java/com/subway/railme/home/API/SubwayService.java
+++ app/src/main/java/com/subway/railme/home/API/SubwayService.java
@@ -1,14 +1,16 @@
 package com.subway.railme.home.API;
 
+import com.subway.railme.home.API.remote.RealtimeStationArrivalResponse;
+import com.tickaroo.tikxml.annotation.Path;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface SubwayService {
     @GET("api/subway/{apiKey}/xml/realtimeStationArrival/0/1/{stationName}")
     Call<RealtimeStationArrivalResponse> getRealtimeStationArrival(
-            @Query("apiKey") String apiKey,
-            @Query("stationName") String stationName
+            @Path("apiKey") String apiKey,
+            @Path("stationName") String stationName
     );
 }
