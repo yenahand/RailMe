f402b4ef255b0aacdaafe70100c4b5a4a1c6631b
4SeokHyeon <tktjrgus@naver.com>
2024-03-27 14:47:41 +0900

    손대지말아요 아무것도

--- app/src/main/java/com/subway/railme/Unit.kt
+++ app/src/main/java/com/subway/railme/Unit.kt
@@ -3,4 +3,5 @@ package com.subway.railme
 object Unit {
     const val API = "59436b514a74706633314b69617558"
     const val BASE_URL ="https://swopenapi.seoul.go.kr/"
+    const val TYPE = "xml"
 }
