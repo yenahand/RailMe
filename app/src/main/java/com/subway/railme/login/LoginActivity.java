0fe10329f04d5cf46a61feb08b0327306df2499e
saseoghyeon <tktjrgus@naver.com>
2024-03-29 00:52:17 +0900

    API 통신 자체는 되는데 I  {"status":500,"code":"ERROR-300","message":"필수 값이 누락되어 있습니다. 요청인자를 참고 하십시오.","link":"","developerMessage":"","total":0}
    이 오류 뜸

--- app/src/main/java/com/subway/railme/login/LoginActivity.java
+++ app/src/main/java/com/subway/railme/login/LoginActivity.java
@@ -5,7 +5,6 @@ package com.subway.railme.login;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.accounts.Account;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,6 +19,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.kakao.sdk.common.KakaoSdk;
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.Account;
 import com.subway.railme.MainActivity;
 import com.subway.railme.databinding.ActivityLoginBinding;
 
@@ -28,6 +29,7 @@ import kotlin.jvm.functions.Function2;
 
 /**
  * 로그인 액티비티  바로 켜지게 & 간편 로그인 텍스트만 누르면 그냥 Main으로 넘어감
+ * 전체적으로 코드의 모듈화가 좋아 보이네요 카카오 간편 로그인과 파이어베이스 회원가입을 하는걸 따로 분리를 해뒀다는 설명이 필요해 보여요
  */
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
@@ -59,6 +61,7 @@ public class LoginActivity extends AppCompatActivity {
                 /**
                  * 솔지히 이건 필요없지 않나요 파베 안쓰잖아
                  * - 기본 로그인에는 파이어베이스를 이용해서 로그인하는 걸로 이용해서 구현한 것이고,swe 카카오 간편 로그인은 따로 간편 로그인 구동할 수 있게 하려고 둘이 따로 한 것입니다!
+                 * 기본 로그인과 카카오 간편 로그인을 같이 하려고 하는것 같군요 그럼 파이어베이스 프로젝트도 tktjrgus11@gmail.com으로 권한줘서 초대 해주세요
                  */
                 // Firebase에 이메일과 비밀번호로 로그인 요청
                 firebaseAuth.signInWithEmailAndPassword(email, password)
@@ -82,7 +85,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        binding.ibkakaologin.setOnClickListener(new View.OnClickListener() {
+        binding.ibKakaoLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(LoginActivity.this)) {
@@ -131,17 +134,17 @@ public class LoginActivity extends AppCompatActivity {
         UserApiClient.getInstance().loginWithKakaoTalk(LoginActivity.this,(oAuthToken, error) -> {
             if(error != null) {
                 Log.e(TAG, "로그인에 실패하였습니다.", error);
-            } else if(oAuthTOken != null) {
+            } /*else if(oAuthTOken != null) {
                 Log.i(TAG, "로그인에 성공하였습니다!(토큰): " + oAuthToken.getAccessToken());
                 getUserInfo();
-            }
+            }*/
             return null;
         });
     }
 
     public void accountLogin() {
         String TAG = "accountLogin()";
-        UserApiCilent.getInstance().loginWithKakaoAccount(LoginActivity.this,(oAuthToken, error) -> {
+        UserApiClient.getInstance().loginWithKakaoAccount(LoginActivity.this,(oAuthToken, error) -> {
             if(error != null) {
                 Log.e(TAG, "로그인에 실패하였습니다.", error);
             } else if (oAuthToken != null) {
