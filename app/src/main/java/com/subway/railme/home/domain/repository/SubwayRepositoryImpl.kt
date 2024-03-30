1d6588f73a41bd0d38b5e913f6c8f33039276d74
saseoghyeon <tktjrgus@naver.com>
2024-03-30 17:02:17 +0900

    응답 데이터 로그

--- app/src/main/java/com/subway/railme/home/domain/repository/SubwayRepositoryImpl.kt
+++ app/src/main/java/com/subway/railme/home/domain/repository/SubwayRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package com.subway.railme.home.domain.repository
 
 
-import com.subway.railme.home.API.dto.RealtimeStationArrival
+import android.util.Log
+import com.subway.railme.Unit.API
+import com.subway.railme.home.domain.dto.RealtimeStationArrival
 import com.subway.railme.home.domain.model.ArrivalModel
 import com.subway.railme.home.subwayapi.RetrofitClient
 import retrofit2.Response
@@ -13,26 +15,30 @@ class SubwayRepositoryImpl(
     override suspend fun getSubwayInfo(statnNm: String): List<ArrivalModel>? {
         try {
 
-            val response: Response<RealtimeStationArrival> = client.api.getSubway(stationName = statnNm)
+            val response: Response<RealtimeStationArrival> = client.api.getSubway(API,
+                stationName = statnNm)
             return if (response.isSuccessful) {
                 val realtimeStationArrival: RealtimeStationArrival? = response.body()
-
-                // null 처리를 수행합니다.
-                realtimeStationArrival?.items?.map { infoItem ->
+                Log.d("SSH", "Response: $realtimeStationArrival")
+                realtimeStationArrival?.realtimeArrivalList?.map { infoItem ->
                     ArrivalModel(
                         subwayId = infoItem.subwayId,
                         trainLineNm = infoItem.trainLineNm,
                         statnId = infoItem.statnId,
                         statnNm = infoItem.statnNm,
                         btrainSttus = infoItem.btrainSttus,
-                        barvlDt = infoItem.barvlDt
+                        barvlDt = infoItem.barvlDt,
+                        arvlMsg2 = infoItem.arvlMsg2,
+                        arvlMsg3 = infoItem.arvlMsg3,
+                        arvlCd = infoItem.arvlCd
                     )
                 } ?: emptyList() // 빈 목록을 반환합니다.
             } else {
-                null // 요청이 실패한 경우 null을 반환합니다.
+                Log.e("SSH", "Request failed with code: ${response.code()}")
+                null
             }
         } catch (e: Exception) {
-            e.printStackTrace()
+            Log.e("SSH", "Exception occurred: ${e.message}", e)
             return null
         }
     }
