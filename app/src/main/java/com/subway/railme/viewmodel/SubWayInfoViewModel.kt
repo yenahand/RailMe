700060ba7e1e015fcc6c4e8b8b5ca9d34dafeda6
saseoghyeon <tktjrgus@naver.com>
2024-03-30 17:02:38 +0900

    UI 적용전 ViewModel 데이터 값 확인

--- app/src/main/java/com/subway/railme/viewmodel/SubWayInfoViewModel.kt
+++ app/src/main/java/com/subway/railme/viewmodel/SubWayInfoViewModel.kt
@@ -6,45 +6,47 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.subway.railme.home.API.dto.RealtimeStationArrival
 import com.subway.railme.home.domain.model.ArrivalModel
 import com.subway.railme.home.domain.repository.SubWayRepository
 import com.subway.railme.home.domain.repository.SubwayRepositoryImpl
 import com.subway.railme.home.subwayapi.RetrofitClient
 import kotlinx.coroutines.launch
-import okhttp3.ResponseBody
 
 class SubWayInfoViewModel(
     private val subWayRepository: SubWayRepository,
-) : ViewModel(){
-    private var subWay :String = ""
+) : ViewModel() {
+    private var subWay: String = ""
 
     private val _searchWay: MutableLiveData<List<ArrivalModel>?> = MutableLiveData()
     val searchWay: LiveData<List<ArrivalModel>?> get() = _searchWay
 
-    init{
+    init {
         viewModelScope.launch {
 
         }
     }
-        fun setSubwayInfo(statnNm:String){
-            Log.d("SubWayInfoViewModel", "Input word: $statnNm")
+
+    fun setSubwayInfo(statnNm: String) {
+        Log.d("SubWayInfoViewModel", "Input word: $statnNm")
         subWay = statnNm
         viewModelScope.launch {
             val arrivalModels = subWayRepository.getSubwayInfo(statnNm)
             _searchWay.value = arrivalModels
+            arrivalModels?.forEach { arrivalModel ->
+                Log.e("SSSS", arrivalModel.toString())
+            }
         }
     }
 
 }
 
-class SubWayInfoViewModelFactory():ViewModelProvider.Factory{
+class SubWayInfoViewModelFactory() : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(SubWayInfoViewModel::class.java)){
+        if (modelClass.isAssignableFrom(SubWayInfoViewModel::class.java)) {
             return SubWayInfoViewModel(
                 SubwayRepositoryImpl(RetrofitClient)
-            )as T
-        }else{
+            ) as T
+        } else {
             throw IllegalArgumentException("Not Found ViewModel Class")
         }
     }
