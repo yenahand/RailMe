f9b3af1c373ca95f61f49bda23231d90c1157112
saseoghyeon <tktjrgus@naver.com>
2024-04-01 00:01:42 +0900

    디자인 필요없는거 변경

--- app/src/main/java/com/subway/railme/myroot/MyRootResultActivity.java
+++ app/src/main/java/com/subway/railme/myroot/MyRootResultActivity.java
@@ -20,7 +20,7 @@ public class MyRootResultActivity extends AppCompatActivity {
     private TextView tvmyresultarrive;
     private TextView tvmyresulttime;
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint({"MissingInflatedId", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
