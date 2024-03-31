2a32efc751f35fae41169342cd9e00cd45530a20
김예나 <tpffl9979@naver.com>
2024-03-31 11:22:20 +0900

    길찾기api 다시 해보고있어요..

---
+++ app/src/main/java/com/subway/railme/myroot/SubPath.java
@@ -0,0 +1,21 @@
+package com.subway.railme.myroot;
+public class SubPath {
+    private String stationName;
+    private String way;
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public String getWay() {
+        return way;
+    }
+
+    public void setWay(String way) {
+        this.way = way;
+    }
+}
---
+++ app/src/main/java/com/subway/railme/myroot/myroot_API/API_StationID/StationID.java
@@ -0,0 +1,25 @@
+package com.subway.railme.myroot.myroot_API.API_StationID;
+
+public class StationID {
+    private int stationID;
+    private String stationName;
+
+    public int getStationID() {
+
+        return stationID;
+    }
+
+    public void setStationID(int stationID) {
+
+        this.stationID = stationID;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+
+        this.stationName = stationName;
+    }
+}
