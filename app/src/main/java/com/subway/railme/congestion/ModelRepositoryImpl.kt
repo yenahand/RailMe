f44fa602913598cfb5e20125c6b77d5d1bc36c09
saseoghyeon <tktjrgus@naver.com>
2024-04-03 23:22:37 +0900

    딥러닝 impl 설계

--- app/src/main/java/com/subway/railme/congestion/ModelRepositoryImpl.kt
+++ app/src/main/java/com/subway/railme/congestion/ModelRepositoryImpl.kt
@@ -1,9 +1,37 @@
 package com.subway.railme.congestion
 
+import android.content.Context
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okio.IOException
 import org.tensorflow.lite.Interpreter
+import java.io.File
 
-class ModelRepositoryImpl():ModelRepository {
+class ModelRepositoryImpl(private val context: Context) : ModelRepository {
     override suspend fun getModel(): List<Interpreter> {
-        TODO("Not yet implemented")
+        val interpreters = mutableListOf<Interpreter>()
+
+        for (i in 1..8) {
+            val modelFileName = "line$i/$i+linemodel.tflite"
+
+            try {
+                val inputStream = context.assets.open(modelFileName)
+                val modelFile = withContext(Dispatchers.IO) {
+                    File.createTempFile("model", null, context.cacheDir)
+                }
+                inputStream.use { input ->
+                    modelFile.outputStream().use { output ->
+                        input.copyTo(output)
+                    }
+                }
+
+                val interpreter = Interpreter(modelFile)
+                interpreters.add(interpreter)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+        return interpreters
     }
 }
