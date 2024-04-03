ba3fe0d9ed254f2b6a5e2c74bdb089d5d1301ffb
Yeahnakim <tpffl9979@naver.com>
2024-04-03 15:32:16 +0900

    실시간 도착정보 창 , 닫기버튼 및 결과 텍스트장 조금씩 수정중입니다

--- app/src/main/java/com/subway/railme/home/dialog/ArrivalInfoDialog.kt
+++ app/src/main/java/com/subway/railme/home/dialog/ArrivalInfoDialog.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.view.Window
+import android.widget.Button
 import android.widget.TextView
 import com.subway.railme.R
 import com.subway.railme.home.domain.model.ArrivalModel
@@ -18,16 +19,27 @@ class ArrivalInfoDialog(
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.dialog_arrival_info)
 
+        val tvStationName = findViewById<TextView>(R.id.tv_stationName)
         val tvArrivalInfo = findViewById<TextView>(R.id.tvArrivalInfo)
-        val stringBuilder = StringBuilder()
-        arrivalModels.forEach { arrivalModel ->
-            stringBuilder.append("지하철 호선: ${arrivalModel.subwayId}\n")
+
+        for (i in 0 until minOf(3, arrivalModels.size)) {
+            val arrivalModel = arrivalModels[i]
+            if (i == 0) {
+                // 지하철역명은 위에 따로 표시되도록 햇습니다
+                tvStationName.text = "${arrivalModel.statnNm}역"
+            }
+            val stringBuilder = StringBuilder(tvArrivalInfo.text)
+            //stringBuilder.append("지하철 호선: ${arrivalModel.subwayId}\n")
             stringBuilder.append("도착지방면: ${arrivalModel.trainLineNm}\n")
-            stringBuilder.append("지하철역ID: ${arrivalModel.statnId}\n")
-            stringBuilder.append("지하철역명: ${arrivalModel.statnNm}\n")
+            //stringBuilder.append("지하철역ID: ${arrivalModel.statnId}\n")
             stringBuilder.append("열차 종류: ${arrivalModel.btrainSttus}\n")
             stringBuilder.append("열차 도착 예정 시간: ${arrivalModel.barvlDt}\n\n")
+            tvArrivalInfo.text = stringBuilder.toString()
         }
-        tvArrivalInfo.text = stringBuilder.toString()
+
+        val btnShutdown = findViewById<Button>(R.id.btn_shutdown)
+        btnShutdown.setOnClickListener {
+            dismiss() // 닫기버튼
+        }
     }
 }
